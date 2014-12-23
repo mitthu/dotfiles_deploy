334ff82e38e97d4aa6c39e8d086adcfc39e65853
Aditya Basu <aditya.ba@directi.com>
2014-12-23 16:17:34 +0530

    Stop update if not installed.

--- update.sh
+++ update.sh
@@ -5,11 +5,20 @@
 # Run:
 # ---
 # curl -sL <link> | /bin/bash
- 
+
+# Status Codes
+STATUS_NOT_INSTALLED=1
+
+# Halt if not installed
+if [[ ! -h $HOME/.merc ]] || [[ ! -d $HOME/.me ]]; then
+	echo "Not installed. Stop."
+	exit $STATUS_NOT_INSTALLED
+fi
+
 source $HOME/.merc
 source "$HOME/.homesick/repos/homeshick/homeshick.sh"
-homeshick pull dotfiles 
- 
+homeshick pull dotfiles
+
 ### Link it all to $HOME ###
 homeshick link --force
 
